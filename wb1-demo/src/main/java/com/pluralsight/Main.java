447509193f79e67d3891ba994afd06954813b70a
Jacob <whatzdylan@gmail.com>
2025-04-14 10:53:39 -0400

    first example of expression using concat operator

--- wb1-demo/src/main/java/com/pluralsight/Main.java
+++ wb1-demo/src/main/java/com/pluralsight/Main.java
@@ -2,6 +2,12 @@ package com.pluralsight;
 
 public class Main {
     public static void main(String[] args) {
+
+        String word1 = "Hello";
+        String word2 = "World";
+        String greeting;
+        greeting = word1 + " " + word2 + "!";
+        /// @author
         System.out.println("Hello, World!");
     }
 }
