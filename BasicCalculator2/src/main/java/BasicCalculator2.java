36f29266b16b26b899436d2f222fa7f75fd0c8c9
Jacob <whatzdylan@gmail.com>
2025-04-15 09:18:07 -0400

    asdf

--- BasicCalculator2/src/main/java/BasicCalculator2.java
+++ BasicCalculator2/src/main/java/BasicCalculator2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class BasicCalculator {
+public class BasicCalculator2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
